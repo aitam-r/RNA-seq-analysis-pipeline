f7688954b476114ac2d5549f6a6dbe82672b0c5b
aitam-r <paul.eth94@gmail.com>
2021-08-19 13:10:14 +0200

    Added instructions single/paired, tested single, maj download
    - single end snakefile seems complete and functional
    - README.org updated in consequence
    - download_dump upgraded to be able to be restarted
    - one can provide the sample names

--- scripts/download_dump.py
+++ scripts/download_dump.py
@@ -1,6 +1,7 @@
 #needs to be run from data/seqs
 
 import subprocess as sp
+import os
 
 Runs = []
 with open("../../SRR_Acc_List.txt", "r") as filin:
@@ -8,11 +9,16 @@ with open("../../SRR_Acc_List.txt", "r") as filin:
 
 
 for r in Runs:
-    sp.run(["prefetch", "-v", r])
+    if not os.path.exists(r + "/" + r + ".sra"):
+        if os.path.exists(r + "/" + r + ".sra.lock"):
+           sp.run(["rm", "-r", r])
+        sp.run(["prefetch", "-v", r])
+    if not os.path.exists(r + ".fastq.gz"):
+        sp.run(["fastq-dump", "--gzip", r])
+        sp.run(["vdb-validate", r])
+
+# Maybe delete folders with the .sra only inside afterwards
 
 for r in Runs:
-    sp.run(["fastq-dump", "--split-files", "--gzip", r])
-
-for r in Runs:
-    sp.run(["vdb-validate", r])
+    sp.run(["rm", "-r", r])
     
