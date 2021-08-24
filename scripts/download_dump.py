03bb4ab7b4ba2e2b5ce283f90210f690cf27e09f
aitam-r <paul.eth94@gmail.com>
2021-08-24 17:07:12 +0200

    multiqc ruled fixed paired

--- scripts/download_dump.py
+++ scripts/download_dump.py
@@ -13,7 +13,7 @@ with open("../../SRR_Acc_List.txt", "r") as filin:
 
 
 for r in Runs:
-    if not os.path.exists(r) and not (os.path.exists(r + "_2.fastq.gz") or os.path.exists(r + "_1.fastq.gz")):
+    if not os.path.exists(r) and not (os.path.exists(r + "_2.fastq.gz") and os.path.exists(r + "_1.fastq.gz")):
         print("Prefetching...")
         sp.run(["prefetch", "-v", r])
     if not (os.path.exists(r + "_2.fastq.gz") and os.path.exists(r + "_1.fastq.gz")):
